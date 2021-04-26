b6050ced4b6a56fd82da6713c19b6705cd40b9d4
Galen Guyer <galen@galenguyer.com>
2021-04-26 16:28:32 -0400

    Formatting and testing opt printing and adding

--- parse.c
+++ parse.c
@@ -9,16 +9,19 @@
 struct option* _options[MAX_OPTS];
 int _opt_count = 0;
 
-void _print_option(struct option *opt) { printf("opt at %p\n", opt); }
+void _print_option(struct option* opt) {
+    printf("opt{short_opt:'%s', long_opt:'%s'}\n", opt->short_opt,
+           opt->long_opt);
+}
 
 void print_options() {
-  for (int i = 0; i < _opt_count; i++) {
-    _print_option(_options[i]);
-  }
+    for (int i = 0; i < _opt_count; i++) {
+        _print_option(_options[i]);
+    }
 }
 
 void add_opt() {
-    struct option opt = { "-h", "-h" };
+    struct option opt = {.short_opt = "-h", .long_opt = "--help"};
     _options[_opt_count] = &opt;
     _opt_count++;
 }
