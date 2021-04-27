42f092251c472939e17e81712fd55a787aca2a98
Galen Guyer <galen@galenguyer.com>
2021-04-27 00:26:33 -0400

    Fix not having space for null terminator in strip_left

--- strings.c
+++ strings.c
@@ -3,13 +3,13 @@
 
 char* strip_left(const char* input, const char delimiter) {
     int start = 0;
-    for(int i = 0; i < strlen(input); i++) {
-        if (input[i] != delimiter) { 
+    for (int i = 0; i < strlen(input); i++) {
+        if (input[i] != delimiter) {
             break;
         }
         start++;
     }
-    char* output = malloc(sizeof(char) * (strlen(input) - start));
-    strncpy(output, input+start, strlen(input) - start);
+    char* output = calloc((strlen(input) - start + 1), sizeof(char));
+    strncpy(output, input + start, strlen(input) - start);
     return output;
 }
